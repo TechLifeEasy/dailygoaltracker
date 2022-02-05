3d03db0dfbebfba12ea3c99b055357f5a7a92bdf
Barun Debnath <barundebnath91@gmail.com>
2022-02-05 11:59:41 +0530

    login signup page done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-
-import './App.css';
-import {useEffect} from 'react';
-import {SingUp} from './Api/user'
+import "./App.css";
+import { useEffect } from "react";
+import { SingUp } from "./Api/user";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
 
 function App() {
-
-  useEffect(()=>{
-
-    SingUp('zeelprajapati@gmail.com','1232232','zeel best');
-  },[])
+  useEffect(() => {
+    SingUp("zeelprajapati@gmail.com", "1232232", "zeel best");
+  }, []);
 
   return (
     <div className="App">
-    Start
+      {/* <SignUp /> */}
+      <LogIn />
     </div>
   );
 }
